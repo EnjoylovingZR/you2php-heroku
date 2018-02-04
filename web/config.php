f7934dc7c0253d57b600b01027a5861a3c4dab72
EnjoylovingZR <13831187076@163.com>
2018-02-04 12:28:44 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAvfqbDvx_lmEcaKTHb-3kE609gEzFMTyQ');
-define('GJ_CODE','HK');
-define('SITE_NAME','EasyTube');
-define('TITLENAME', 'EasyTube');
-define('EN2DEKEY','AIzaSyAvfqbDvx');
-define('EMAIL', 'baobao1015774675@gmail.com');
+define('APIKEY', 'AIzaSyAvfqbDvx_lmEcaKTHb-3kE609gEzFMTyQ');//这里改成您的api key
+define('GJ_CODE', 'HK');//国家代码
+define('SITE_NAME', 'Youtube');//网站名字
+define('TITLENAME', 'Youtube');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EMAIL', 'baobao1015774675@gmail.com');//邮箱
 ?>
